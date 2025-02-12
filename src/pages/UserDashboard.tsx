ad6c00220274d491abeaf535274b9270663ad406
skynguyenlee <skynguyenlee@gmail.com>
2025-02-12 16:38:06 +0700

    12/2/2025-BuiChiNguyen-Task9-RequestPage

--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -4,6 +4,8 @@ import Navbar from "../components/Narbar";
 import Approval from "../components/Approval";
 import Request from "../components/Request";
 import Finance from "../components/Finance";
+import RequestDetails from "../components/RequestDetails";
+import CreateRequest from "../components/CreateRequest";
 
 
 
@@ -18,19 +20,22 @@ export default function UserDashBoard() {
         </div>
         {/* Menu */}
         <div className="mt-6 w-full">
-          <Menu/>
+          <Menu />
         </div>
       </div>
 
       {/* Content Area */}
       <div className="w-[82%] md:w-[90%] lg:w-[82%] xl:w-[82%] flex-1 bg-[#F7F8FA] overflow-scroll">
-      <Navbar/>
-      <Routes>
-          <Route path="/approvals" element={<Approval/>} />
-          <Route path="/claimrequest" element={<Request/>} />
-          <Route path="/finance" element={<Finance/>} />
-      </Routes>
-        
+        <Navbar />
+        <Routes>
+          <Route path="/approvals" element={<Approval />} />
+          <Route path="/claimrequest" element={<Request />} />
+          <Route path="/finance" element={<Finance />} />
+
+          <Route path="/request-detail/:id" element={<RequestDetails />} />
+          <Route path="/create-request" element={<CreateRequest />} />
+        </Routes>
+
       </div>
     </div>
   );
