05fef632d1012ef727300d8b7809845e06bb4686
QHuyyyyy <huy120904@gmail.com>
2025-02-13 13:42:54 +0700

    add lazy loading for userroute

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,37 +1,26 @@
-import { lazy, Suspense } from 'react';
-import Header from '../layout/header';
-import Banner from '../layout/banner';
 
-const ServicesSection = lazy(() => import('../components/home/ServicesSection'));
-const GlobalPresence = lazy(() => import('../components/home/GlobalPresence')); 
-const CultureSection = lazy(() => import('../components/home/CultureSection'));
-const Footer = lazy(() => import('../layout/footer'));
+import Header from "../layout/header";
+import Banner from "../layout/banner";
+import ServicesSection from "../components/home/ServicesSection";
+import GlobalPresence from "../components/home/GlobalPresence";
+import CultureSection from "../components/home/CultureSection";
+import Footer from "../layout/footer";
 
-const LoadingSection = () => (
-  <div className="h-96 flex items-center justify-center">
-    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
-  </div>
-);
+
+
+
 
 const Homepage = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <Banner />
-      <Suspense fallback={<LoadingSection />}>
-        <ServicesSection />
-      </Suspense>
-      <Suspense fallback={<LoadingSection />}>
-        <GlobalPresence />
-      </Suspense>
-      <Suspense fallback={<LoadingSection />}>
-        <CultureSection />
-      </Suspense>
-      <Suspense fallback={<LoadingSection />}>
-        <Footer />
-      </Suspense>
+      <ServicesSection />
+      <GlobalPresence />
+      <CultureSection />
+      <Footer />
     </div>
   );
 };
 
-export default Homepage; 
+export default Homepage;
