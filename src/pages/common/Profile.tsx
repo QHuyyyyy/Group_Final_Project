378ff817efe7c322e02d767268206f47e850938d
YunCt123 <yunct1234@gmail.com>
2025-02-18 10:20:28 +0700

    18-02-anhtd-updatemenu

--- src/pages/common/Profile.tsx
+++ src/pages/common/Profile.tsx
@@ -1,6 +1,5 @@
 import { Card,  Badge, Statistic, Row, Col, Avatar } from 'antd';
 import { UserOutlined,  ClockCircleOutlined,FileTextOutlined } from '@ant-design/icons';
-import Header from '../../layout/header';
 
 interface Staff {
   id: string;
