7e0ce0721cbb86a9373f0f9c82fb51e66d094758
QHuyyyyy <huy120904@gmail.com>
2025-02-20 15:06:54 +0700

    20/2-Huy-completed services api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,23 +6,43 @@ interface createUser {
   user_name: string;
   role_code: string;
 }
+interface SearchParams {
+  searchCondition: {
+    keyword?: string;
+    role_code?: string;
+    is_blocked?: boolean;
+    is_delete?: boolean;
+    is_verified?: string;
+  };
+  pageInfo: {
+    pageNum: number;
+    pageSize: number;
+  };
+}
+interface UpdateUserData {
+  email?: string;
+  user_name?: string;
+}
 export const userService = {
   createUser: async (userData: createUser) => {
     const response = await api.post('/api/users', userData);
-    return response.data;
+    console.log("fetch data:",response.data.success)
+    return response.data.data;
   },
 
-  searchUsers: async (params: any) => {
+  searchUsers: async (params: SearchParams) => {
     const response = await api.post('/api/users/search', params);
-    return response.data;
+    console.log("fetch data:",response.data.success)
+    return response.data.data;
   },
 
   getUserById: async (id: string) => {
     const response = await api.get(`/api/users/${id}`);
-    return response.data;
+    console.log("fetch data:",response.data.success)
+    return response.data.data;
   },
 
-  updateUser: async (id: string, userData: any) => {
+  updateUser: async (id: string, userData: UpdateUserData) => {
     const response = await api.put(`/api/users/${id}`, userData);
     return response.data;
   },
@@ -32,18 +52,18 @@ export const userService = {
     return response.data;
   },
 
-  changePassword: async (id: string, passwords: any) => {
-    const response = await api.put(`/api/users/change-password`, passwords);
+  changePassword: async ( old_password: string, new_password: string) => {
+    const response = await api.put(`/api/users/change-password`, {old_password,new_password});
     return response.data;
   },
 
-  changeStatus: async (id: string, status: string) => {
-    const response = await api.put(`/api/users/change-status`, { id, status });
+  changeStatus: async ( user_id:string,old_status: string, new_status: string) => {
+    const response = await api.put(`/api/users/change-status`, { user_id, old_status,new_status });
     return response.data;
   },
 
-  changeRole: async (id: string, role: string) => {
-    const response = await api.put(`/api/users/change-role`, { id, role });
+  changeRole: async ( user_id:string,old_role: string, new_role: string) => {
+    const response = await api.put(`/api/users/change-role`, { user_id, old_role,new_role });
     return response.data;
   }
 };
