4a8d268952c0b6bf367723838ca5c1d7c09df20e
huathuong2209 <thuonghua220903@gmail.com>
2025-03-03 15:03:46 +0700

    3/3/2025_Thuong_handle_responses

--- src/services/roleService.ts
+++ src/services/roleService.ts
@@ -1,24 +1,18 @@
 import api from '../api/axios';
-
-interface Role {
-  _id: string;
-  role_code: string; 
-  role_name: string;
-  description: string;
-  is_deleted: boolean;
-  created_at: string;
-  updated_at: string;
-  __v: number;
-}
+import { Role, RoleTypes } from '../models/RoleModel';
 
 export const roleService = {
   // Lấy tất cả roles hoặc tìm kiếm theo từ khóa
-  getAllRoles: async (keyword?: string): Promise<Role[]> => {
-    const response = await api.get('/api/roles/get-all', {
-      params: { keyword }
-    });
-    console.log("fetch data:",response.data.data)
-    return response.data.data;
+  getAllRoles: async (keyword?: RoleTypes): Promise<Role[]> => {
+    try {
+      const response = await api.get('/api/roles/get-all', {
+        params: { keyword }
+      });
+      console.log("fetch data:", response.data.data);
+      return response.data.data;
+    } catch (error) {
+      console.error("Error fetching roles:", error);
+      throw error;
+    }
   },
-
 };
