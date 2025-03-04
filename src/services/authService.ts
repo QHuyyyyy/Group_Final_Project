ca42650088422ccce315a16933f69d7f888ea1db
QHuyyyyy <huy120904@gmail.com>
2025-03-04 10:13:30 +0700

    4/2-Huy-use common axios services

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,45 +1,45 @@
 // src/services/authService.ts
-import api from "../api/axios";
+import  { apiUtils } from "../api/axios";
 import { ApiResponse } from "../models/ApiResponse";
 import { Credentials, Email, Info, Token } from "../models/AuthModel";
 
 export const authService = {
   login: async (credentials: Credentials) : Promise<ApiResponse<Token>> => {
-    const response = await api.post("/api/auth", credentials);
+    const response = await apiUtils.post<ApiResponse<Token>>("/api/auth", credentials);
     console.log("fetch data:", response.data);
     return response.data;
   },     
   getinfo: async () : Promise<ApiResponse<Info>> => {
-    const response = await api.get("/api/auth");
+    const response = await apiUtils.get<ApiResponse<Info>>("/api/auth");
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   logout: async () : Promise<ApiResponse<null>> => {
-    const response = await api.post("/api/auth/logout");
+    const response = await apiUtils.post<ApiResponse<null>>("/api/auth/logout");
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   verifyToken: async (token: Token) : Promise<ApiResponse<null>> => {
-    const response = await api.post("/api/auth/verify-token", token);
+    const response = await apiUtils.post<ApiResponse<null>>("/api/auth/verify-token", token);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   resendToken: async (email: Email) : Promise<ApiResponse<null>> =>  {
-    const response = await api.post("/api/auth/resend-token", email);
+    const response = await apiUtils.post<ApiResponse<null>>("/api/auth/resend-token", email);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   forgotPassword: async (email: Email): Promise<ApiResponse<null>> => {
-    const response = await api.put("/api/auth/forgot-password", email);
+    const response = await apiUtils.put<ApiResponse<null>>("/api/auth/forgot-password", email);
     console.log("fetch data:", response.data);
     return response.data;
   },
   triggerVerify: async (email: Email): Promise<ApiResponse<null>> => {
-    const response = await api.post("/api/auth/trigger-verify-token", email);
+    const response = await apiUtils.post<ApiResponse<null>>("/api/auth/trigger-verify-token", email);
     console.log("fetch data:", response.data); 
     return response.data;
   },
--- src/services/claimService.ts
+++ src/services/claimService.ts
@@ -1,60 +1,60 @@
-import api from "../api/axios";
+import api, { apiUtils } from "../api/axios";
 import { ApiResponse } from "../models/ApiResponse";
 import { CreateClaimRequest, SearchParams, SearchResponse, UpdateClaimRequest,  ChangeClaimStatusRequest, CreateClaim, ClaimById } from "../models/ClaimModel";
 
 export const claimService = {
   // Tạo claim mới
   createClaim: async (claimData: CreateClaimRequest): Promise<ApiResponse<CreateClaim>> => {
-    const response = await api.post('/api/claims', claimData);
+    const response = await apiUtils.post<ApiResponse<CreateClaim>>('/api/claims', claimData);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Tìm kiếm claims với phân trang
   searchClaims: async (params: SearchParams) => {
-    const response = await api.post('/api/claims/search', params);
+    const response = await  apiUtils.post('/api/claims/search', params);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Tìm kiếm claims theo người yêu cầu
   searchClaimsByClaimer: async (params: SearchParams): Promise<ApiResponse<SearchResponse>> => {
-    const response = await api.post('/api/claims/claimer-search', params);
+    const response = await  apiUtils.post<ApiResponse<SearchResponse>>('/api/claims/claimer-search', params);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Tìm kiếm claims cần phê duyệt
   searchClaimsForApproval: async (params: SearchParams): Promise<ApiResponse<SearchResponse>> => {
-    const response = await api.post('/api/claims/approval-search', params);
+    const response = await  apiUtils.post<ApiResponse<SearchResponse>>('/api/claims/approval-search', params);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Tìm kiếm claims cho bộ phận tài chính
   searchClaimsForFinance: async (params: SearchParams): Promise<ApiResponse<SearchResponse>> => {
-    const response = await api.post('/api/claims/finance-search', params);
+    const response = await  apiUtils.post<ApiResponse<SearchResponse>>('/api/claims/finance-search', params);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Lấy thông tin claim theo ID
   getClaimById: async (id: string): Promise<ApiResponse<ClaimById>> => {
-    const response = await api.get(`/api/claims/${id}`);
+    const response = await  apiUtils.get<ApiResponse<ClaimById>>(`/api/claims/${id}`);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Cập nhật thông tin claim
   updateClaim: async (id: string, claimData: UpdateClaimRequest) => {
-    const response = await api.put(`/api/claims/${id}`, claimData);
+    const response = await  apiUtils.put(`/api/claims/${id}`, claimData);
     console.log("fetch data:", response.data);
     return response.data;
   },
 
   // Thay đổi trạng thái claim
   changeClaimStatus: async (requestBody: ChangeClaimStatusRequest): Promise<ApiResponse<null>> => {
-    const response = await api.put('/api/claims/change-status', requestBody);
+    const response = await  api.put('/api/claims/change-status', requestBody);
 
     console.log("fetch data:", response.data.data);
     return response.data.data;
