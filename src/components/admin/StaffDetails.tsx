d1fedf5ba2f6733523f2860d2c7deb765dd7c0ed
huathuong2209 <thuonghua220903@gmail.com>
2025-03-06 10:33:19 +0700

    6/3/2025-Thuong-use_model

--- src/components/admin/StaffDetails.tsx
+++ src/components/admin/StaffDetails.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Modal,  Tag } from 'antd';
-
-interface Staff {
-  _id: string;
-  email: string;
-  user_name: string;
-  role_code: string;
-  is_verified: boolean;
-  verification_token: string;
-  verification_token_expires: string;
-  token_version: number;
-  is_blocked: boolean;
-  created_at: string;
-  updated_at: string;
-  is_deleted: boolean;
-  __v: number;
-}
+import { Modal, Tag } from 'antd';
+import { UserData } from '../../models/UserModel';
 
 interface StaffDetailsProps {
   visible: boolean;
-  staff: Staff;
+  staff: UserData;
   onClose: () => void;
 }
 
