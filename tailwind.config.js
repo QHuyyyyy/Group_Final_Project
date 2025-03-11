0e205b11423cafdbe5bdd1eb50455ed31ddfe0cd
AkaiHiHi <tronglmse180005@fpt.edu.vn>
2025-03-11 16:53:37 +0700

    11/3-tronglm-edit banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",  
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bubble': 'bubble 7s linear infinite',
+      },
+      keyframes: {
+        bubble: {
+          '0%': { transform: 'translateY(0)', opacity: '0' },
+          '50%': { opacity: '1' },
+          '70%': { opacity: '1' },
+          '100%': { transform: 'translateY(-80vh)', opacity: '0' },
+        }
+      }
+    },
   },
   plugins: [],
   important: true,
