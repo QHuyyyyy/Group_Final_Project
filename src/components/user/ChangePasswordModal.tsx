0f7fa222458a7730001622b3b782051cb3454ad2
huathuong2209 <thuonghua220903@gmail.com>
2025-04-03 11:32:54 +0700

    03/04/2025-Thuong-fix_toast

--- src/components/user/ChangePasswordModal.tsx
+++ src/components/user/ChangePasswordModal.tsx
@@ -3,7 +3,7 @@ import { Form, Input, Button, Modal } from 'antd';
 import { userService } from '../../services/user.service';
 import { InputVaild } from '../../constants/InputVaild';
 import { KeyOutlined, SecurityScanOutlined } from '@ant-design/icons';
-import { toast } from 'react-toastify';
+import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface ChangePasswordModalProps {
@@ -35,6 +35,19 @@ const ChangePasswordModal: React.FC<ChangePasswordModalProps> = ({ visible, onCa
   };
 
   return (
+    <>
+    <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     <Modal
       title={<div style={{ textAlign: 'center', width: '100%' }}>Change Password</div>}
       open={visible}
@@ -79,6 +92,7 @@ const ChangePasswordModal: React.FC<ChangePasswordModalProps> = ({ visible, onCa
         </Form.Item>
       </Form>
     </Modal>
+    </>
   );
 };
 
